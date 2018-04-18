f4c00816860e6b0a5fbf3b24cc41032d26bbb1e8
Nick Mistretta <nickmistretta9@gmail.com>
2018-04-18 16:51:32 -0400

    Initial commit, sodoku file reader created

---
+++ SodokuSolver/Workers/SodokuFileReader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace SodokuSolver
+{
+    public class SodokuFileReader
+    {
+        public int[,] ReadFile(string filename)
+        {
+            int[,] sodokuBoard = new int[9, 9];
+            try
+            {
+                var sodokuBoardLines = File.ReadAllLines(filename);
+                var row = 0;
+                foreach(var sodokuBoardLine in sodokuBoardLines)
+                {
+                    string[] sodokuBoardLineElements = sodokuBoardLine.Split('|').Skip(1).Take(9).ToArray();
+
+                    int col = 0;
+                    foreach(var sodokuBoardLineElement in sodokuBoardLineElements)
+                    {
+                        sodokuBoard[row, col] = sodokuBoardLineElement.Equals(" ") ? 0 : Convert.ToInt16(sodokuBoardLineElement);
+                        col++;
+                    }
+                    row++;
+                }
+            } catch(Exception ex)
+            {
+                throw new Exception("Something went wrong while reading the file: " + ex.Message);
+            }
+            return sodokuBoard;
+        }
+    }
+}
